a8d8b9c6b4498cbc9da213581a3f339e5fa4d81c
Christian Amauger <christian.amauger@gmail.com>
2019-12-03 13:53:38 -0500

    menu fix

--- header.php
+++ header.php
@@ -28,6 +28,45 @@
 
 
 <body <?php body_class(); ?>>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Rotary - Club de Québec-Est <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Le club</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Missions et objectifs</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Activités</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Bottin</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+    </ul>
+    <button class="my-2 my-lg-0">
+                                    Devenir membre
+    </button>
+  </div>
+</nav>
+
+
+
+
+
+
     <nav id="menupage" class="shadow-bottom navbar navbar-default navbar-fixed-top" role="navigation">
         <div class="container">
             <div class="navbar-header">

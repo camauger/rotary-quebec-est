8c5b7767d51c3ab736bcc984af36a256511f0755
Christian Amauger <christian.amauger@gmail.com>
2019-12-03 13:57:33 -0500

    menu fix

--- header.php
+++ header.php
@@ -30,35 +30,35 @@
 <body <?php body_class(); ?>>
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="#">
+                                    <img src="https://rotary-quebecest.org/contenu/wp-content/themes/rotary/images/rotary-qcest-logo-full.png" alt="Rotary - Club de Québec-Est">
+
+        </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Rotary - Club de Québec-Est <span class="sr-only">(current)</span></a>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://rotary-quebecest.org/le-club/mot-du-president/">Le club</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Le club</a>
+                    <a class="nav-link" href="https://rotary-quebecest.org/mission-et-objectifs/mission/">Missions et objectifs</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Missions et objectifs</a>
+                    <a class="nav-link" href="https://rotary-quebecest.org/activites/calendrier/">Activités</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Activités</a>
+                    <a class="nav-link" href="https://rotary-quebecest.org/fr/membres/bottin.php">Bottin</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Bottin</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contact</a>
+                    <a class="nav-link" href="#https://rotary-quebecest.org/contact/>Contact</a>
                 </li>
             </ul>
-            <button class="my-2 my-lg-0">
+            <a class="btn btn-primary my-2 my-lg-0" href="https://rotary-quebecest.org/fr/_new/demande_adhesion.php">
                 Devenir membre
-            </button>
+            </a>
         </div>
     </nav>
 

c643ff516e9b4f1da07aa8c7df5d582f408a4dbf
Christian Amauger <christian.amauger@gmail.com>
2019-12-04 13:58:49 -0500

    promo fix

--- page-accueil.php
+++ page-accueil.php
@@ -66,28 +66,25 @@ $the_query_xs = new WP_Query($args);
 <div class="">
     <div class="container">
         <div class="row">
-            <div class="col-md-12">
-                <div class="promo-cite-joie">
 
-                    <div>
+            <div>
 
-                        <div class="col-md-6">
-                            <div class="margin-bottom-1">
-                                <h2>Cité Joie, de nature à faire du bien</h2>
-                                <p><img class="wp-image-946 size-full alignleft" src="https://rotary-quebecest.org/contenu/prospecteur/uploads/2015/01/citejoie_logo_noir-site-web.jpg" alt="" width="190" height="190" srcset="https://rotary-quebecest.org/contenu/wp-content/uploads/2015/01/citejoie_logo_noir-site-web.jpg 200w, https://rotary-quebecest.org/contenu/wp-content/uploads/2015/01/citejoie_logo_noir-site-web-150x150.jpg 150w, https://rotary-quebecest.org/contenu/wp-content/uploads/2015/01/citejoie_logo_noir-site-web-120x120.jpg 120w" sizes="(max-width: 190px) 100vw, 190px"></p>
-                                <p>Cité Joie est un centre de vacances et répit pour personnes handicapées situé à Lac-Beauport, près de Québec. Parrainé par le club Rotary Québec-Est, le centre&nbsp;reçoit chaque année plus de 2&nbsp;600 personnes handicapées&nbsp;intellectuellement et/ou physiquement, âgées de 3&nbsp;à&nbsp;80 ans, et&nbsp;leur offre un accueil chaleureux en milieu naturel.</p>
-                                <p><a href="https://rotary-quebecest.org/mission-et-objectifs/citejoie/">Cliquez ici pour en savoir plus sur&nbsp;Cité Joie.</a></p>
-                                <p>&nbsp;</p>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="margin-top-2 embed-responsive embed-responsive-16by9">
-                                <iframe class="embed-responsive-item" src="//www.youtube.com/embed/Qi2os1Tddd8?rel=0" allowfullscreen=""></iframe>
-                            </div>
-                        </div>
-
+                <div class="col-md-6">
+                    <div class="margin-bottom-1">
+                        <h2>Cité Joie, de nature à faire du bien</h2>
+                        <p><img class="wp-image-946 size-full alignleft" src="https://rotary-quebecest.org/contenu/prospecteur/uploads/2015/01/citejoie_logo_noir-site-web.jpg" alt="" width="190" height="190" srcset="https://rotary-quebecest.org/contenu/wp-content/uploads/2015/01/citejoie_logo_noir-site-web.jpg 200w, https://rotary-quebecest.org/contenu/wp-content/uploads/2015/01/citejoie_logo_noir-site-web-150x150.jpg 150w, https://rotary-quebecest.org/contenu/wp-content/uploads/2015/01/citejoie_logo_noir-site-web-120x120.jpg 120w" sizes="(max-width: 190px) 100vw, 190px"></p>
+                        <p>Cité Joie est un centre de vacances et répit pour personnes handicapées situé à Lac-Beauport, près de Québec. Parrainé par le club Rotary Québec-Est, le centre&nbsp;reçoit chaque année plus de 2&nbsp;600 personnes handicapées&nbsp;intellectuellement et/ou physiquement, âgées de 3&nbsp;à&nbsp;80 ans, et&nbsp;leur offre un accueil chaleureux en milieu naturel.</p>
+                        <p><a href="https://rotary-quebecest.org/mission-et-objectifs/citejoie/">Cliquez ici pour en savoir plus sur&nbsp;Cité Joie.</a></p>
+                        <p>&nbsp;</p>
                     </div>
                 </div>
+                <div class="col-md-6">
+                    <div class="margin-top-2 embed-responsive embed-responsive-16by9">
+                        <iframe class="embed-responsive-item" src="//www.youtube.com/embed/Qi2os1Tddd8?rel=0" allowfullscreen=""></iframe>
+                    </div>
+                </div>
+
+
             </div>
         </div>
     </div>
